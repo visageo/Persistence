0485167e408e40bf62275c5cabee644956368c60
visageo <81907663+visageo@users.noreply.github.com>
2021-08-03 11:46:19 +0200

    Update baza.sql

--- baza.sql
+++ baza.sql
@@ -1,27 +1,5 @@
--- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
---
--- Host: localhost    Database: ormassignment
--- ------------------------------------------------------
--- Server version	5.7.31
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `employee`
---
-
 DROP TABLE IF EXISTS `employee`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `employee` (
   `person_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
@@ -29,25 +7,17 @@ CREATE TABLE `employee` (
   `salary` int(11) NOT NULL,
   PRIMARY KEY (`person_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `employee`
---
 
 LOCK TABLES `employee` WRITE;
-/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-INSERT INTO `employee` VALUES (7,'John',28,890),(8,'Mark',24,600),(9,'Patrick',32,1380),(10,'Greg',45,1880),(12,'Ian',44,2300),(30,'Nick',21,600),(27,'Mathew',27,940),(26,'Arnold',22,1000),(29,'Mic the Mic',26,8700),(25,'Carl',33,1890);
-/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+INSERT INTO `employee` VALUES
+  (7,'John',28,890),
+  (8,'Mark',24,600),
+  (9,'Patrick',32,1380),
+  (10,'Greg',45,1880),
+  (12,'Ian',44,2300),
+  (30,'Nick',21,600),
+  (27,'Mathew',27,940),
+  (26,'Arnold',22,1000),
+  (25,'Carl',33,1890);
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-02-19 18:54:16
